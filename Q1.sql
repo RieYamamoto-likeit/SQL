422ab732ac1f9e6a48def8c5bf6ed115f7de5773
RieYamamoto <yama.ri95.02@gmail.com>
2018-09-14 18:17:26 +0900

    Q7まで完了

---
+++ Q1.sql
@@ -0,0 +1,8 @@
+
+'CREATE TABLE item_category (category_id int, category_name varchar(256));'
+CREATE TABLE item_category (category_id int NOT NULL AUTO_INCREMENT,category_name varchar(256) NOT NULL, PRIMARY KEY (`category_id`));
+
+
+'INSERT INTO item_category VALUES(NOT NULL,AUTO_INCREMENT, NOT NULL);'
+
+
---
+++ Q2.sql
@@ -0,0 +1,2 @@
+CREATE TABLE item(item_id int NOT NULL AUTO_INCREMENT,item_name varchar(256) NOT NULL,item_price int NOT NULL,category_id int,PRIMARY KEY(`item_id`));
+
